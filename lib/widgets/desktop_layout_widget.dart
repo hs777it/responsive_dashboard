53b76809ea7c3d010d91d78d3d39aaa834c0eeac
hs07 <hs777it@gmail.com>
2024-06-01 00:07:31 +0300

    create all expensess widget

--- lib/widgets/desktop_layout_widget.dart
+++ lib/widgets/desktop_layout_widget.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:responsive_dashboard/widgets/custom_drawer.dart';
+
+import 'all_expenses.dart';
 
 class DesktopLayout extends StatelessWidget {
   const DesktopLayout({super.key});
@@ -10,6 +13,14 @@ class DesktopLayout extends StatelessWidget {
     return const Row(
       children: [
         Expanded(child: CustomDrawer()),
+        SizedBox(width: 32),
+        Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                Expanded(child: AllExpenses()),
+              ],
+            )),
       ],
     );
   }
