fe57dc7cd9a98089ac8f8736b6dd312915920069
hs07 <hs777it@gmail.com>
2024-06-01 00:35:49 +0300

    all expensess header done

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
+import 'all_expenses_header.dart';
+
 class AllExpenses extends StatelessWidget {
   const AllExpenses({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: const Column(
+        children: [
+          AllExpensesHeader(),
+        ],
+      ),
+    );
   }
 }
